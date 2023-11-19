862f7bcbb53e2acfac0de75d7c5e64b2a27f002a
jafar bino <binocoder@gmail.com>
2023-11-19 23:02:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    return words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    }).join(' ');
+  }
 }
+
+// Test cases
+console.log(Formatter.capitalize('hello')); // Output: 'Hello'
+console.log(Formatter.sanitize('hello#world')); // Output: 'helloworld'
+console.log(Formatter.titleize('the lord of the rings')); // Output: 'The Lord of the Rings'
